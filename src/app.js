451ee5ca8bc832702d81efb9db712680340445a8
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-07 11:59:19 +0900

    스켈레톤 코드 준비완료
    Co-authored-by: New-mean <New-mean@users.noreply.github.com>
    Co-authored-by: NterChoi <choi16289@naver.com>
    Co-authored-by: Jacob_moon <Jacob-moon@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+
+const app = express();
+const PORT = 7777;
+
+app.use(LogMiddleware);
+app.use(express.json());
+// 라우트 자리
+app.use(ErrorHandlingMiddleware);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
