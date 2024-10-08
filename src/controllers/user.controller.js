6c7f076dd74cc91d0c56761394ab292de61d3b71
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-08 23:44:05 +0900

    [add] 컨트롤러 로그인 로직 추가
    진행중입니다.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import { UserService } from "../services/user.service";
+
 // User의 컨트롤러(Controller)역할을 하는 클래스
 export class UserController {
     constructor(userService){
@@ -31,8 +33,30 @@ export class UserController {
     } catch (err) {
       
         res.status(500).json({
-            message:"회원가입 실패!",
+            message:'회원가입 실패!',
         });
     }
   };
+
+  //로그인 로직 
+  userLogin = async(req,res,next) =>{
+    try{
+      const {
+        email,
+        password
+      }=req.body;
+      const loginUser = await this.userService.login(
+        email,
+        password,
+      );
+      return res.status(201).json({
+        message:'로그인 성공!',
+        data:loginUser,
+      });
+    }catch(err){
+      res.status(500).json({
+        message:'로그인 실패!',
+      });
+    }
+  }
 }
