0260a8bfb8f7ed224cdb1f843fa198c2125d2df8
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-08 23:43:47 +0900

    [add] 로그인 로직 추가
    진행중입니다

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -3,14 +3,14 @@ export class UserRepository{
   constructor(prisma) {
     this.prisma = prisma
   }
-
+  //이메일 중복 체크 
     checkEmail= async (email) =>{
       const existingUser = await this.prisma.user.findUnique({
         where:{email}
       });
       return existingUser;
     }
-
+    // 유저 생성
     createdUser = async (email, hashedPassword, name, address, type) => {
     
         const createdUser = await this.prisma.user.create({
@@ -24,4 +24,5 @@ export class UserRepository{
         });
         return createdUser;
       };
+
 }
