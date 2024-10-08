e4fbe17aa120410fb6d685494844b43c531ae73c
New-mean <dmb08075@naver.com>
2024-10-09 00:19:37 +0900

    test

--- src/app.js
+++ src/app.js
@@ -2,19 +2,14 @@ import express from 'express';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import userRouter from './routes/user.router.js';
-// import sotreRouter from './routes/store.router.js';
-// import orderRouter from './routes/order.router.js';
-// import reviewRouter from './routes/review.router.js';
 
 const app = express();
 const PORT = 7777;
 
-
-
 app.use(LogMiddleware);
 app.use(express.json());
 //app.use('/api'); //라우터 넣는 곳
-app.use('/api',userRouter);
+app.use('/api', userRouter);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
