e1de1e37bf58d94d2b015cb024bdb2ffed895c24
SungRok1231 <tnsfhr100@gmail.com>
2024-10-10 16:34:09 +0900

    deleteMenu 초기세팅 - 김성록

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -49,7 +49,7 @@ export class MenuController {
   deleteMenu = async (req, res, next) => {
     //삭제할 메뉴의 id
     //유저.바디의 이메일과 req.바디의 이메일 일시하면 삭제가능
-    //
+
     console.log(user.email);
   };
 }
