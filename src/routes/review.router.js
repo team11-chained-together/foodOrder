5bb36822cc9b88bfa3f5b4e4214f4f7634f344a8
New-mean <dmb08075@naver.com>
2024-10-10 15:05:32 +0900

    review router

---
+++ src/routes/review.router.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+import { ReviewController } from '../controllers/review.controller.js';
+import { ReviewService } from '../services/review.service.js';
+import { ReviewRepository } from '../repositories/review.repository.js';
+
+const router = express.Router();
+
+const reviewRepository = new ReviewRepository(prisma);
+const reviewService = new ReviewService(reviewRepository);
+const reviewController = new ReviewController(reviewService);
+
+router.post('/reviews', reviewController.postReview);
+router.put('/reviews', reviewController.putReview);
+router.delete('/reviews/:reviewId', reviewController.deleteReview);
