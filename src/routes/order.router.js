3937dbae57454b49d407243c2a089abda6df8d3b
New-mean <dmb08075@naver.com>
2024-10-10 23:04:58 +0900

    [add] order

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
+import { OrderController } from '../controllers/order.controller.js';
+import { OrderService } from '../services/order.service.js';
+import { OrderRepository } from '../repositories/order.repository.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-export default router;
+const orderRepository = new OrderRepository(prisma);
+const orderService = new OrderService(orderRepository);
+const orderController = new OrderController(orderService);
+
+router.post('/orders', orderController.postOrder);
