467126ed6d72e8fafa55f93ab68e18d632d8cec2
New-mean <dmb08075@naver.com>
2024-10-10 14:40:55 +0900

    dev pull

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 import express from 'express';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
-<<<<<<< HEAD
 import storeRouter from './routes/store.router.js';
-=======
 import userRouter from './routes/user.router.js';
->>>>>>> shinminjae
 
 const app = express();
 const PORT = 7777;
 
 app.use(LogMiddleware);
 app.use(express.json());
-<<<<<<< HEAD
-app.use('/api', [storeRouter]); //라우터 넣는 곳
-=======
-//app.use('/api'); //라우터 넣는 곳
-app.use('/api', userRouter);
->>>>>>> shinminjae
+app.use('/api', [storeRouter, userRouter]); //라우터 넣는 곳
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
