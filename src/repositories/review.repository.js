55cc045bc07f5b98207b6250a52cef5d31945c19
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-11 22:39:05 +0900

    fix: 오타 수정

--- src/repositories/review.repository.js
+++ src/repositories/review.repository.js
@@ -3,7 +3,7 @@ export class ReviewRepository {
     this.prisma = prisma;
   }
   //리뷰 작성 (생성)
-  creatReview = async (userId, comment, rate) => {
+  createReview = async (userId, comment, rate) => {
     const createdReview = await this.prisma.review.create({
       where: {
         storeId: +storeId,
@@ -17,7 +17,7 @@ export class ReviewRepository {
   };
 
   //리뷰 수정
-  updateReveiw = async (reviewId, comment, rate) => {
+  updateReview = async (reviewId, comment, rate) => {
     const updatedReview = await this.prisma.review.update({
       where: {
         reviewId: +reviewId,
