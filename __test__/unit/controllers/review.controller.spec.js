61681e0aa5007bd74143d446d421b91e02ab484a
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-12 01:38:18 +0900

    fix: 스토어 서비스 유닛 테스트 수정
    Co-authored-by: New-mean <New-mean@users.noreply.github.com>

--- __test__/unit/controllers/review.controller.spec.js
+++ __test__/unit/controllers/review.controller.spec.js
@@ -1,67 +1,142 @@
-import{jest,test} from '@jest/globals';
-import{ReviewController} from '../../../src/controllers/review.controller.js';
+import { jest, test } from '@jest/globals';
+import { ReviewController } from '../../../src/controllers/review.controller.js';
 
-const mockReviewService ={
-    createReview : jest.fn(),
+const mockReviewService = {
+  createReview: jest.fn(),
+  updateReview: jest.fn(),
 };
 
 const mockRequest = {
-    body : jest.fn(),
+  body: jest.fn(),
 };
 
-const mockResponse ={
-    json:jest.fn(),
-    status:jest.fn(),
+const mockResponse = {
+  json: jest.fn(),
+  status: jest.fn(),
 };
 
 const mockNext = jest.fn();
 
 const reviewController = new ReviewController(mockReviewService);
 
-describe('Review Controller unit test', ()=>{
-    beforeEach(()=>{
-        jest.resetAllMocks();
+describe('리뷰 컨트롤러 유닛 테스트', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
 
-        mockResponse.status.mockReturnValue(mockResponse);
+    mockResponse.status.mockReturnValue(mockResponse);
+  });
+
+  test('리뷰 작성 성공 테스트', async () => {
+    const createReviewBodyParams = {
+      storeId: 1,
+      comment: 'JMT',
+      rate: 3,
+    };
+    mockRequest.body = createReviewBodyParams;
+
+    const createReviewValue = {
+      reviewId: 1,
+      userId: 1,
+      storeId: 1,
+      ...createReviewBodyParams,
+      createdAt: updateReview.createdAt,
+      updatedAt: updateReview.updatedAt,
+    };
+    mockReviewService.createReview.mockReturnValue(createReviewValue);
+
+    await reviewController.createReview(mockRequest, mockResponse, mockNext);
+    expect(mockReviewService.createReview).toHaveBeenCalledTimes(1);
+    expect(mockReviewService.createReview).toHaveBeenCallWith(
+      createReviewBodyParams.reviewId,
+      createReviewBodyParams.userId,
+      createReviewBodyParams.storeId,
+      createReviewBodyParams.comment,
+      createReviewBodyParams.rate,
+      createReviewBodyParams.createdAt,
+      createReviewBodyParams.updatedAt,
+    );
+
+    expect(mockRequest.status).toHaveBeenCalledTimes(1);
+    expect(mockRequest.status).toHaveBeenCallWith(201);
+
+    expect(mockResponse.json).toHaveBeenCallWith(1);
+    expect(mockResponse.json).toHaveBeenCallWith({
+      message: '리뷰가 정상적으로 작성되었습니다.',
+      data: createReviewValue,
     });
+  });
 
-    test('createReview Method By Success', async ()=>{
-        const createReviewBodyParams = {
-            storeId:1,
-            comment:'JMT',
-            rate:3
-        };
-        mockRequest.body = createReviewBodyParams;
+  test('리뷰 업데이트 성공 테스트', async () => {
+    const updateReviewRequestBodyParams = {
+      reviewId: 1,
+      comment: 'JWT123',
+      rate: 2,
+    };
 
-        const createReviewValue = {
-          reviewId:1,
-          userId:1,
-          storeId:1,
-          ...createReviewValue,
-          createdAt:updateReview.createdAt,
-          updatedAt:updateReview.updatedAt,
-        }
-        mockReviewService.createReview.mockReturnValue(createReviewValue);
+    mockRequest.body = updateReviewRequestBodyParams;
 
-        await reviewController.createReview(mockRequest,mockResponse,mockNext);
-        expect(mockReviewService.createReview).toHaveBeenCalledTimes(1);
-        expect(mockReviewService.createReview).toHaveBeenCallWith(
-        createReviewBodyParams.reviewId,
-        createReviewBodyParams.userId,
-        createReviewBodyParams.storeId,
-        createReviewBodyParams.comment,
-        createReviewBodyParams.rate,
-        createReviewBodyParams.createdAt,
-        createReviewBodyParams.updatedAt,
-        );
-        
-        expect(mockRequest.status).toHaveBeenCalledTimes(1);
-        expect(mockRequest.status).toHaveBeenCallWith(201);
+    const updateReviewReturnValue = {
+      reviewId: 1,
+      userId: 1,
+      storeId: 1,
+      comment: 'JWT12341234',
+      rate: 4,
+      createdAt: new Date().toString,
+      updatedAt: new Date().toString,
+    };
 
-        expect(mockResponse.json).toHaveBeenCallWith(1);
-        expect(mockResponse.json).toHaveBeenCallWith({
-            message: '리뷰가 정상적으로 작성되었습니다.',
-            data: createReviewValue
-        });
+    mockReviewService.updateReview.mockReturnValue(updateReviewReturnValue);
+
+    await reviewController.putReview(mockRequest, mockResponse, mockNext);
+    expect(mockReviewService.updateReview).toHaveBeenCalledTimes(1);
+    expect(mockReviewService.updateReview).toHaveBeenCallWith(
+      updateReviewRequestBodyParams.reviewId,
+      updateReviewRequestBodyParams.userId,
+      updateReviewRequestBodyParams.storeId,
+      updateReviewRequestBodyParams.comment,
+      updateReviewRequestBodyParams.rate,
+      updateReviewRequestBodyParams.createdAt,
+      updateReviewRequestBodyParams.updatedAt,
+    );
+
+    expect(mockResponse.status).toHaveBeenCalledTimes(1);
+    expect(mockResponse.status).toHaveBeenCallWith(201);
+
+    expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    expect(mockResponse.json).toHaveBeenCallWith({
+      data: updateReviewReturnValue,
     });
+  });
+
+  test('리뷰 삭제 성공 테스트', async () => {
+    const deleteReviewRequestBodyParams = {
+      reviewId: 1,
+    };
+
+    mockRequest.body = deleteReviewRequestBodyParams;
+
+    const deleteReviewReturnValue = {
+      userId: 1,
+      reviewId: 1,
+      storeId: 1,
+      comment: 'JWT',
+    };
+
+    mockReviewService.deleteReview.mockReturnValue(deleteReviewReturnValue);
+    await reviewController.deleteReview(mockRequest, mockResponse, mockNext);
+    expect(mockReviewService.deleteReview).toHaveBeenCalledTimes(1);
+    expect(mockReviewService.deleteReview).toHaveBeenCallWith(
+      deleteReviewRequestBodyParams.userId,
+      deleteReviewRequestBodyParams.reviewId,
+      deleteReviewRequestBodyParams.storeId,
+      deleteReviewRequestBodyParams.comment,
+    );
+  });
+  expect(mockResponse.status).toHaveBeenCalledTimes(1);
+  expect(mockResponse.status).toHaveBeenCallWith(200);
+
+  expect(mockResponse.json).toHaveBeenCalledTimes(1);
+  expect(mockResponse.json).toHaveBeenCallWith({
+    message: '삭제되었습니다.',
+  });
 });
