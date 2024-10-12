59f6a1d5524799bb4334c4310242939f99415d56
New-mean <dmb08075@naver.com>
2024-10-13 04:03:03 +0900

    [update]nodemailer

--- config/testnodemailer.js
+++ config/testnodemailer.js
@@ -1,13 +1,17 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {user , pass } = process.env;
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    port: 587,
-    host: 'smtp.gmail.com',
-    secure: false,
-    requireTLS: true,
-    auth: {
-      user: 'coconarooroo@gmail.com',
-      pass: 'vjrb vbow xwas ugyz',
-    },
-  });
+        service: 'gmail',
+        port: 587,
+        host: 'smtp.gmail.com',
+        secure: false,
+        requireTLS: true,
+        auth: {
+          user: user,
+          pass: pass,
+        },
+      });
 export default transporter;
--- src/controllers/testnodemailer.controller.js
+++ src/controllers/testnodemailer.controller.js
@@ -6,7 +6,7 @@ export class TestNodeMailerController {
         const {email} = req.body;
         const testnodemailer = await this.testNodeMailerService.testnodemailer(email);
         
-        return res.status(200).json({data :testnodemailer});
+        return res.status(200).json({testnodemailer});
 
 }
 }
