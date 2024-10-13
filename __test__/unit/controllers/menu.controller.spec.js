fb4b35ba546746bddb0307398df7f6043570af16
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-13 18:28:32 +0900

    [fix] menucontroller  Test
    4.menu.controller test
    
    1. const mockMenuService = {
      createMenu: jest.fn(),
      updateMenu: jest.fn(),
      deleteMenu: jest.fn(),
    };
    에서 deleteMenu: jest.fn(), 부분 추가했습니다.
    
    2.
    const mockRequest = {
      body: {},
      user:{},
    }; 에서 jest.fn(), 대신 {} 로받아오는것으로 수정했는데 req 에선 (jest.fn는 함수 호출을 추적할때 사용하는데 여기선 필요없음)
    필요하지 않아서 제거 했습니다.
    res.json()이나 res.status() 같은 응답 메서드의 호출 여부를 확인하려면, jest.fn()을 사용해야 한다고 합니다.
    3. test 코드 들에서 mockRequest.body = createMenuRequestBodyParams;
    에서 mockRequest.user = { userId: 1, isOwner: true }; 추가해서 유저 정보를 추가했습니다 .
    4. 마찬가지로
     createdAt: new Date().toString(),
     updatedAt: new Date().toString(),
    
    에서 toString 을 toString() 로 수정했습니다.
    5.업데이트 파람스에서 마찬가지로 Type -> isOwner 로 바꾸고 파람스에서 객체 menuId 추가해서 정상적으로 받게 했습니다.
    
    //추가로 Test 에 기능 구현된 몇 가지 더 추가해야합니다.
    현재 생성 과 업데이트 부분만 되어있습니다.

--- __test__/unit/controllers/menu.controller.spec.js
+++ __test__/unit/controllers/menu.controller.spec.js
@@ -4,10 +4,12 @@ import { MenuController } from '../../../src/controllers/menu.controller';
 const mockMenuService = {
   createMenu: jest.fn(),
   updateMenu: jest.fn(),
+  deleteMenu: jest.fn(),
 };
 
 const mockRequest = {
-  body: jest.fn(),
+  body: {},
+  user:{},
 };
 
 const mockResponse = {
@@ -37,13 +39,14 @@ describe('Menu Controller Unit Test', () => {
       stock: 30,
     };
     mockRequest.body = createMenuRequestBodyParams;
+    mockRequest.user = { userId: 1, isOwner: true };
 
     // Service 계층에서 구현된 createMenu 메서드를 실행했을때, 반환되는 데이터 형식
     const createdMenuReturnValue = {
       menuId: 1,
       ...createMenuRequestBodyParams,
-      createdAt: new Date().toString,
-      updatedAt: new Date().toString,
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
     };
 
     mockMenuService.createMenu.mockReturnValue(createdMenuReturnValue);
@@ -51,7 +54,7 @@ describe('Menu Controller Unit Test', () => {
     await menuController.createMenu(mockRequest, mockResponse, mockNext);
     expect(mockMenuService.createMenu).toHaveBeenCalledTimes(1);
     expect(mockMenuService.createMenu).toHaveBeenCalledWith(
-      createMenuRequestBodyParams.userId,
+      mockRequest.user.userId,
       createMenuRequestBodyParams.menuName,
       createMenuRequestBodyParams.image,
       createMenuRequestBodyParams.price,
@@ -72,7 +75,8 @@ describe('Menu Controller Unit Test', () => {
   test('updateMethod By Success', async () => {
     const updateMenuRequestBodyParams = {
       userId: 1,
-      type: true,
+      isOwner: true,
+      menuId:1,
       menuName: '수정할 메뉴이름',
       image: '수정할 이미지 URL',
       price: 1000,
@@ -80,13 +84,14 @@ describe('Menu Controller Unit Test', () => {
     };
 
     mockRequest.body = updateMenuRequestBodyParams;
+    mockRequest.user = { userId: 1, isOwner: true };
 
     // service 계층에서 구현된 updateMenu 메소드를 실행하였을 때, 반환되는 데이터 형식
     const updatedMenuReturnValue = {
       menuId: 1,
       ...updateMenuRequestBodyParams,
-      createdAt: new Date().toString,
-      updatedAt: new Date().toString,
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
     };
 
     mockMenuService.updateMenu.mockReturnValue(updatedMenuReturnValue);
@@ -94,7 +99,8 @@ describe('Menu Controller Unit Test', () => {
     await menuController.updateMenu(mockRequest, mockResponse, mockNext);
     expect(mockMenuService.updateMenu).toHaveBeenCalledTimes(1);
     expect(mockMenuService.updateMenu).toHaveBeenCalledWith(
-      updateMenuRequestBodyParams.userId,
+      mockRequest.user.userId,
+      updateMenuRequestBodyParams.menuId,
       updateMenuRequestBodyParams.menuName,
       updateMenuRequestBodyParams.image,
       updateMenuRequestBodyParams.price,
