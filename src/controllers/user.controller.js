1ef5f0aa69b7a148346e9a9501c036ef353ab8b0
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-10-13 17:16:25 +0900

    [add] 회원가입 부분 비밀번호 확인 추가

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -63,6 +63,7 @@ export class UserController {
       const createdUser = await this.userService.signUp(
         signUpUser.email,
         signUpUser.password,
+        signUpUser.confirmPassword,
         signUpUser.name,
         signUpUser.address,
         signUpUser.isOwner,
