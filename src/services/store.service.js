51d98b3cac9f34572ee8a29ac8a096f56b5eb892
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-15 01:41:32 +0900

    fix: Service 커스텀 에러 구현
    Co-authored-by: NterChoi <127270767+NterChoi@users.noreply.github.com>

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -1,15 +1,19 @@
+import {
+  SearchStoreValidation,
+  CreateStoreValidation,
+  StoreValidation,
+  GetStoreValidation,
+} from '../utils/validators/service/storeValidator.js';
+
 export class StoreService {
   constructor(storeRepository) {
     this.storeRepository = storeRepository;
   }
 
   searchStores = async (search) => {
-    console.log(search);
     const store = await this.storeRepository.searchStores(search);
-
-    if (store.length === 0) {
-      throw new Error('검색 결과가 없습니다.');
-    }
+    const storeValidation = new SearchStoreValidation(store);
+    storeValidation.validate();
 
     return store.map((store) => ({
       storeId: store.storeId,
@@ -25,9 +29,8 @@ export class StoreService {
 
   createStore = async (userId, storeName, location, foodType) => {
     const store = await this.storeRepository.findStoreByUserId(userId);
-    if (store) {
-      throw new Error('이미 보유하고 있는 식당이 있습니다.');
-    }
+    const storeValidation = new CreateStoreValidation(store);
+    storeValidation.validate();
 
     const createdStore = await this.storeRepository.createStore(
       userId,
@@ -50,10 +53,8 @@ export class StoreService {
 
   updateStore = async (userId, storeName, foodType, location) => {
     const store = await this.storeRepository.findStoreByUserId(userId);
-
-    if (!store) {
-      throw new Error('보유하고 있는 식당이 없습니다, 식당을 만들어주세요.');
-    }
+    const storeValidation = new StoreValidation(store);
+    storeValidation.validate();
 
     const updatedStore = await this.storeRepository.updateStore(
       userId,
@@ -75,10 +76,8 @@ export class StoreService {
 
   deleteStore = async (userId) => {
     const store = await this.storeRepository.findStoreByUserId(userId);
-
-    if (!store) {
-      throw new Error('보유하고 있는 식당이 없습니다.');
-    }
+    const storeValidation = new StoreValidation(store);
+    storeValidation.validate();
 
     const deletedStore = await this.storeRepository.deleteStore(userId);
 
@@ -92,14 +91,12 @@ export class StoreService {
 
   // 모든 가게 목록 조회
   getStore = async () => {
-    const store = await this.storeRepository.findStore();
-
-    if (!store) {
-      throw new Error('존재하는 음식점이 없습니다.');
-    }
+    const stores = await this.storeRepository.findStore();
+    const getStoreValidation = new GetStoreValidation(stores);
+    getStoreValidation.validate();
 
     return {
-      store: store,
+      store: stores,
     };
   };
 }
---
+++ src/utils/validators/service/checkOrderValidator.js
@@ -0,0 +1,66 @@
+import { ValidationError } from '../../errors/ValidationError.js';
+
+export class CheckOrderValidation {
+  constructor(storeData, orderData) {
+    this.storeData = storeData;
+    this.orderData = orderData;
+  }
+
+  validate() {
+    this.validateStoreData();
+    this.validateOrderData();
+  }
+
+  validateStoreData() {
+    if (!this.storeData) {
+      throw new ValidationError('상점이 없습니다. 상점을 만들어주세요.');
+    }
+  }
+
+  validateOrderData() {
+    if (!this.orderData) {
+      throw new ValidationError('주문이 없습니다.');
+    }
+  }
+}
+
+export class CheckMyOrderValidation {
+  constructor(orderData) {
+    this.orderData = orderData;
+  }
+
+  validate() {
+    this.validateOrderData();
+  }
+
+  //TODO: findMany로 빈배열이 나오지만 에러가 안뜸 문제 해결해야함
+  validateOrderData() {
+    if (!this.orderData) {
+      throw new ValidationError('내가 주문한 목록이 없습니다.');
+    }
+  }
+}
+
+export class UpdateOrderStatementValidation {
+  constructor(storeId, order) {
+    this.storeId = storeId;
+    this.order = order;
+  }
+
+  validate() {
+    this.validateUpdateOrderStatement();
+    this.validateStoreIdOrderId();
+  }
+
+  validateUpdateOrderStatement() {
+    if (!this.order) {
+      throw new ValidationError('해당 주문은 존재하지 않습니다.');
+    }
+  }
+
+  validateStoreIdOrderId() {
+    if (this.storeId !== this.order.storeId) {
+      throw new ValidationError('해당하는 오더는 사장님 가게의 주문목록에 없습니다.');
+    }
+  }
+}
